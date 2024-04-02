82b09e177cf786e04620aa179b318c29d500263b
Theo Gravity <theo@switchboard.app>
2024-04-02 12:45:42 -0700

    Use biome

--- jest.config.js
+++ jest.config.js
@@ -1,17 +1,13 @@
 module.exports = {
-  roots: ['<rootDir>/test'],
-  testMatch: [
-    '**/__tests__/**/*.+(ts|tsx|js)',
-    '**/?(*.)+(spec|test).+(ts|tsx|js)',
-  ],
+  roots: ["<rootDir>/test"],
+  testMatch: ["**/__tests__/**/*.+(ts|tsx|js)", "**/?(*.)+(spec|test).+(ts|tsx|js)"],
   transform: {
-    '^.+\\.(ts|tsx)$': 'ts-jest',
-    '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
-      './config/fileTransformer.js',
+    "^.+\\.(ts|tsx)$": ["ts-jest", { tsConfig: "tsconfig.json", diagnostics: { exclude: ["**"] } }],
+    "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$":
+      "./config/fileTransformer.js",
   },
-  globals: {
-    'ts-jest': {
-      tsconfig: 'tsconfig.json'
-    }
-  }
-}
+  testEnvironment: "node",
+  moduleNameMapper: {
+    electron: "<rootDir>/__mocks__/electron.js",
+  },
+};
